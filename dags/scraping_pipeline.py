135c6418d3f131e25b41f58602aa636985484056
dhruvkanasara11 <dhruvamitkanasara11@gmail.com>
2026-02-20 00:23:51 -0500

    First commit

--- dags/scraping_pipeline.py
+++ dags/scraping_pipeline.py
@@ -1,4 +1,4 @@
-from airflowexit import DAG
+from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.operators.bash import BashOperator
 from datetime import datetime, timedelta
